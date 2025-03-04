5ae97fa9deb4004f5682d0c1980113a60ff30aea
Marcus Rangel <marcus.vbrangel@gmail.com>
2025-03-03 21:45:49 -0300

    feat: implemented project 02

---
+++ 02-projeto/src/main/java/com/mvbr/springsecurity/config/security/ApplicationSecurityConfig.java
@@ -0,0 +1,74 @@
+package com.mvbr.springsecurity.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class ApplicationSecurityConfig {
+
+    // -------------------------------------------------------------------------------------------------
+
+    private static final String ROLE_READ = "read";
+
+    private final CustomAuthenticationProvider customAuthenticationProvider;
+
+    public ApplicationSecurityConfig(CustomAuthenticationProvider customAuthenticationProvider) {
+        this.customAuthenticationProvider = customAuthenticationProvider;
+    }
+
+    @Bean
+    UserDetailsService userDetailsService() {
+
+        var user = User.withUsername("joao")
+            .password("123456")
+            .authorities(ROLE_READ)
+            .build();
+
+        return new InMemoryUserDetailsManager(user);
+
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+
+        return NoOpPasswordEncoder.getInstance();
+
+    }
+
+    // -------------------------------------------------------------------------------------------------
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity.httpBasic(Customizer.withDefaults());
+
+        httpSecurity.authenticationProvider(customAuthenticationProvider);
+
+        httpSecurity.authorizeHttpRequests(
+            authorize -> authorize.anyRequest().authenticated()
+        );
+
+        return httpSecurity.build();
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
